40706cbafd85a53d4cb7f4e46fb1ef34f2979e1c
yuteng@wahaha.com.cn <yuteng759251>
2022-02-18 10:20:42 +0800

    过滤器

--- src/main/java/com/scaffolding/Application.java
+++ src/main/java/com/scaffolding/Application.java
@@ -3,14 +3,15 @@ package com.scaffolding;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 /**
  * @author yt
  */
 @SpringBootApplication
 @MapperScan(basePackages = "com.scaffolding.mapper")
+@ServletComponentScan(value = {"com.scaffolding.config.filter"})
 public class Application {
-
 
     public static void main(String[] args) {
 
---
+++ src/main/java/com/scaffolding/config/FilterConfig.java
@@ -0,0 +1,28 @@
+package com.scaffolding.config;
+
+import com.scaffolding.config.filter.CrosFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+
+/**
+ * @author whh-yt
+ */
+@Configuration
+public class FilterConfig {
+
+    @Autowired
+    private CrosFilter crosFilter;
+
+    @Bean
+    public FilterRegistrationBean<CrosFilter> crosFilter() {
+        FilterRegistrationBean<CrosFilter> registration = new FilterRegistrationBean<>();
+        registration.setFilter(crosFilter);
+        registration.addUrlPatterns("/*");
+        registration.setName("crosFilter");
+        registration.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        return registration;
+    }
+}
