2e5f63e2058dbb39e1a78be346ac5a7d78b04479
yuteng@wahaha.com.cn <yuteng759251>
2022-04-27 08:37:54 +0800

    format

--- src/main/java/com/scaffolding/service/app/mine/vo/CouponsVO.java
+++
@@ -1,9 +0,0 @@
-package com.scaffolding.service.app.mine.vo;
-
-/**
- * @Description: TODO
- * @Author yt
- * @Date 2021/12/28 11:42 上午
- */
-public class CouponsVO {
-}
--- src/main/java/com/scaffolding/service/shopCart/command/CartCommand.java
+++
@@ -1,8 +0,0 @@
-package com.scaffolding.service.shopCart.command;
-
-import com.whhim.hxh.service.shopCart.enums.OperationCartEnum;
-
-public interface CartCommand {
-
-    void execute(OperationCartEnum operationCartEnum);
-}
